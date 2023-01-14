7beafacf67155c483a3055290275c024f288c5c4
Wesley Persad <26929215+wesleypersad@users.noreply.github.com>
2023-01-14 17:46:55 +0000

    Code refactored and some bugs fixed

--- functions/functions.js
+++ functions/functions.js
@@ -8,12 +8,12 @@ function swapCanvases(){
         ctx.style.display='none';
         ctx2.style.display='block';
         btn.textContent = 'Plot Atmospheric Sensors';
-        atmos = 0;
+        atmos = false;
     }else{
         ctx.style.display='block';
         ctx2.style.display='none';
         btn.textContent = 'Plot Liquid Sensors';
-        atmos = 1;
+        atmos = true;
     };
     sessionStorage.setItem("atmos", atmos);
 }
@@ -69,4 +69,186 @@ function getThingData(numVals) {
         });
     
     return [temperatures, humidities, gasppms, pressures, levels, turbidities,  humAlarms,  gasAlarms, levAlarms, turAlarms, timestamps, timestamps2];
+};
+
+// get IoT data and plot
+function plotData() {
+    // get the number of values and what plot from session storage
+    let numVals = parseInt(sessionStorage.getItem("numValues")) || 100;
+    let atmos = sessionStorage.getItem("atmos") || true;
+    sessionStorage.setItem("atmos", atmos);
+
+    // can use getThingData function
+    // get and import the data using getData fetch
+    let [temperatures, humidities, gasppms, pressures, levels, turbidities, humAlarms, gasAlarms, levAlarms,  turAlarms,  timestamps,  timestamps2] =  getThingData(numVals);
+
+    // set button element
+    if (atmos) {
+        btn.textContent = 'Plot Liquid Sensors';
+        ctx.style.display='block';
+        ctx2.style.display='none';
+    } else {
+        btn.textContent = 'Plot Atmospheric Sensors';
+        ctx.style.display='none';
+        ctx2.style.display='block';
+    };
+
+    // time stamps for atmospheric plots
+    var xValues = timestamps;
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xValues,
+            datasets: [{
+                data: temperatures,
+                borderColor: "green",
+                fill: false,
+                label: 'Temperature degC',
+                yAxisID: 'TempHumid',
+            },{
+                data: humidities,
+                borderColor: "blue",
+                fill: false,
+                label: 'Humidity %',
+                yAxisID: 'TempHumid',
+            },{
+                data: gasppms,
+                borderColor: "magenta",
+                fill: false,
+                label: 'Gas ppm',
+                yAxisID: 'Gas',
+            },{
+                data: pressures,
+                borderColor: "cyan",
+                fill: false,
+                label: 'Atm Bar',
+                yAxisID: 'Pressure',
+            },{
+                data: levAlarms,
+                borderColor: "red",
+                fill: false,
+                label: 'Humidity Alarm',
+                yAxisID: 'Alarms',
+            },{
+                data: turAlarms,
+                borderColor: "gold",
+                fill: false,
+                label: 'Gas Alarm',
+                yAxisID: 'Alarms',
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'ATMOSPHERIC SENSOR VALUES',
+                }
+            },
+            legend: {display: true},
+            layout: {
+                    padding: 10
+                    },
+            scales: {
+                xValues: {
+                    type: 'time',
+                    title: {
+                    display: true,
+                    text: 'Date'
+                    },
+                    time: {
+                        unit: 'hour'
+                    }
+                },
+                'TempHumid': {
+                    position: 'left',
+                    min: 0,
+                    max: 100
+                },
+                'Gas': {
+                    position: 'left'
+                },
+                'Pressure': {
+                    position: 'left'
+                },
+                'Alarms': {
+                    position: 'right',
+                    min: 0,
+                    max: 1
+                }
+            }
+        },
+    });
+
+    // add a second one which may have a different timebase
+    var xValues = timestamps2;
+    new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: xValues,
+            datasets: [{
+                data: levels,
+                borderColor: "green",
+                fill: false,
+                label: 'Level %',
+                yAxisID: 'Level',
+            },{
+                data: turbidities,
+                borderColor: "blue",
+                fill: false,
+                label: 'Tubidity',
+                yAxisID: 'Turbidity',
+            },{
+                data: levAlarms,
+                borderColor: "red",
+                fill: false,
+                label: 'Level Alarm',
+                yAxisID: 'Alarms',
+            },{
+                data: turAlarms,
+                borderColor: "gold",
+                fill: false,
+                label: 'Turbidity Alarm',
+                yAxisID: 'Alarms',
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'LIQUID SENSOR VALUES',
+                }
+            },
+            legend: {display: true},
+            layout: {
+                    padding: 10
+                    },
+            scales: {
+                xValues: {
+                    type: 'time',
+                    title: {
+                    display: true,
+                    text: 'Date'
+                    },
+                    time: {
+                        unit: 'hour'
+                    }
+                },
+                'Level': {
+                    position: 'left',
+                    min: 0,
+                    max: 100
+                },
+                'Turbidity': {
+                    position: 'left',
+                    min: 0,
+                    max: 1000
+                },
+                'Alarms': {
+                    position: 'right',
+                    min: 0,
+                    max: 1
+                }
+            }
+        },
+    });
 };
